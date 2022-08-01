422e4a061223b3439b92e9e37b35e92dc74c91f7
ljc@UbuntuLocal <1926963927@qq.com>
2022-08-01 22:14:48 +0800

    remove 的bug 等待解决

--- src/include/storage/page/hash_table_bucket_page.h
+++ src/include/storage/page/hash_table_bucket_page.h
@@ -18,6 +18,7 @@
 #include "common/config.h"
 #include "storage/index/int_comparator.h"
 #include "storage/page/hash_table_page_defs.h"
+#include "common/logger.h"
 
 namespace bustub {
 /**
@@ -138,13 +139,25 @@ class HashTableBucketPage {
    */
   void PrintBucket();
 
+  // 将char表示的数的index位数字位置为0
+  void RemoveBit(char* value, int index) {
+    char bit = static_cast<char>(1 << index);
+    char mask = 0;
+    // ^ 异或操作
+    // ～ 桉位取反
+    mask = static_cast<char>((~mask) ^ bit);
+    *value =static_cast<char>(*value & mask) ;
+  }
  private:
   // For more on BUCKET_ARRAY_SIZE see storage/page/hash_table_page_defs.h
+  // 表示数组该位是否被使用过，可用来提前结束循环。假设顺序寻找数组中的元素，若occupied_[i]为0代表后面没元素了，可以中止寻找了。
   char occupied_[(BUCKET_ARRAY_SIZE - 1) / 8 + 1];
   // 0 if tombstone/brand new (never occupied), 1 otherwise.
+  // 示数组该位当前是否存在元素。当需要删除某个元素时，将readable_置为0，occupied_不变。
   char readable_[(BUCKET_ARRAY_SIZE - 1) / 8 + 1];
   // Do not add any members below array_, as they will overlap.
-  MappingType array_[0];
+  // 零长数组入门 ： https://blog.csdn.net/gatieme/article/details/64131322
+  MappingType array_[0]; // 0长数组作为数组名，不占用存储空间，仅仅是个标记；但是可以用这个名字获取键值对
 };
 
 }  // namespace bustub
--- src/storage/page/hash_table_bucket_page.cpp
+++ src/storage/page/hash_table_bucket_page.cpp
@@ -11,37 +11,97 @@
 //===----------------------------------------------------------------------===//
 
 #include "storage/page/hash_table_bucket_page.h"
+#include <cassert>
+#include <iostream>
+#include <utility>
 #include "common/logger.h"
 #include "common/util/hash_util.h"
 #include "storage/index/generic_key.h"
 #include "storage/index/hash_comparator.h"
+#include "storage/page/hash_table_page_defs.h"
 #include "storage/table/tmp_tuple.h"
 
 namespace bustub {
+// 注意： 实现的是不重复key-value对的bucket页
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BUCKET_TYPE::GetValue(KeyType key, KeyComparator cmp, std::vector<ValueType> *result) {
   return false;
 }
 
+// 注意 ： 如果桶满或者 
+// 键重复都返回false ！ 在遍历空位时应该判断是否重复
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BUCKET_TYPE::Insert(KeyType key, ValueType value, KeyComparator cmp) {
-  return true;
+  int readable_size = sizeof(readable_);
+  assert(readable_size == (BUCKET_ARRAY_SIZE - 1) / 8 + 1);
+  std::cout << "Inserting key = " << key << " : value = " << value << std::endl;
+  for(int array_index = 0; array_index <= readable_size; array_index++) {
+     char reading_bytes = readable_[array_index];
+     for(int byte_idx = 0; byte_idx <= 7; byte_idx++) {
+        if( static_cast<bool>(reading_bytes & (0B10000000 >> byte_idx))){
+          //某一位已经被占用，判断是否重复,使用cmp比较
+          if(cmp(array_[array_index * 8 + byte_idx].first, key) == 0) {
+            return false;
+          }
+          //不重复则继续寻找下一个位置
+          continue;
+        }
+        //找到空位，插入即可
+        LOG_DEBUG("Found free space, array_index = %d, byte_index = %d",array_index, byte_idx);
+        array_[array_index * 8 + byte_idx] = std::pair<KeyType, ValueType>(key,value);
+        readable_[array_index] = readable_[array_index] | (0B10000000 >> byte_idx);
+        //还要修改 occupied数组,将对应的位置1即可
+        occupied_[array_index] = occupied_[array_index] | (0B10000000 >> byte_idx);
+        return true;
+     }
+  }
+  return false;
 }
 
+
+/**
+  * Removes a key and value.
+  *
+  * @return true if removed, false if not found
+  */
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BUCKET_TYPE::Remove(KeyType key, ValueType value, KeyComparator cmp) {
+  int readable_size = sizeof(readable_);
+  assert(readable_size == (BUCKET_ARRAY_SIZE - 1) / 8 + 1);
+  
+  for(int array_index = 0; array_index <= readable_size; array_index++) {
+    char reading_bytes = readable_[array_index];
+    char occupired_bytes = occupied_[array_index];
+    for(int byte_idx = 0; byte_idx <= 7; byte_idx++) {
+      if(! static_cast<bool>(occupired_bytes & (0B10000000 >> byte_idx))){
+        //加速探测过程
+        //occupied 的某个为为0 ， 表示这个为没有使用过，那么数组之后的元素也不可能使用过，直接返回false
+        return false;
+      }
+
+      if(static_cast<bool>(reading_bytes & (0B10000000 >> byte_idx))){
+        //这个位置还有元素，查看它的元素是否是要删除的元素
+        if( cmp(array_[array_index * 8 + byte_idx].first , key)){
+          //如果是则删除它，只需要将readable数组对应的位设置为0即可
+          RemoveBit(&readable_[array_index],7 - byte_idx);
+          assert((readable_[array_index] & (0B10000000 >> byte_idx)) == 0);
+          return true;
+        }
+      }
+    }
+  }
   return false;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 KeyType HASH_TABLE_BUCKET_TYPE::KeyAt(uint32_t bucket_idx) const {
-  return {};
+  return array_[bucket_idx].first;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 ValueType HASH_TABLE_BUCKET_TYPE::ValueAt(uint32_t bucket_idx) const {
-  return {};
+  return array_[bucket_idx].second;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
@@ -49,7 +109,10 @@ void HASH_TABLE_BUCKET_TYPE::RemoveAt(uint32_t bucket_idx) {}
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BUCKET_TYPE::IsOccupied(uint32_t bucket_idx) const {
-  return false;
+  int byte_idx = static_cast<int>(bucket_idx / 8);
+  int bit_idx =static_cast<int>(bucket_idx - byte_idx * 8);
+  char byte = occupied_[byte_idx];
+  return (byte & (0B10000000 >> bit_idx)) != 0;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
@@ -57,7 +120,10 @@ void HASH_TABLE_BUCKET_TYPE::SetOccupied(uint32_t bucket_idx) {}
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
 bool HASH_TABLE_BUCKET_TYPE::IsReadable(uint32_t bucket_idx) const {
-  return false;
+  int byte_idx = static_cast<int>(bucket_idx / 8);
+  int bit_idx =static_cast<int>(bucket_idx - byte_idx * 8);
+  char byte = readable_[byte_idx];
+  return (byte & (0B10000000 >> bit_idx)) != 0;
 }
 
 template <typename KeyType, typename ValueType, typename KeyComparator>
