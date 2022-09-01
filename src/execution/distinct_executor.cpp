98485fac2d1bb8af3879b58b17ca5f92e714588f
ljc@UbuntuLocal <1926963927@qq.com>
2022-09-01 23:16:18 +0800

    本地测试全部通过

--- src/execution/distinct_executor.cpp
+++ src/execution/distinct_executor.cpp
@@ -11,15 +11,51 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/distinct_executor.h"
+#include <cstdint>
+#include <vector>
+#include "catalog/schema.h"
+#include "common/logger.h"
+#include "execution/expressions/column_value_expression.h"
+#include "storage/table/tuple.h"
 
 namespace bustub {
 
 DistinctExecutor::DistinctExecutor(ExecutorContext *exec_ctx, const DistinctPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void DistinctExecutor::Init() {}
+void DistinctExecutor::Init() {
+  child_executor_->Init();
+  set_.clear();
+  Tuple tuple;
+  RID rid;
+  auto schema = plan_->OutputSchema();
+  DistinKey distinct_key;
+  while (child_executor_->Next(&tuple, &rid)) {
+    std::vector<Value> values;
+    values.reserve(plan_->OutputSchema()->GetColumnCount());
+    for (uint32_t i = 0; i < plan_->OutputSchema()->GetColumnCount(); i++) {
+      values.push_back(tuple.GetValue(schema, i));
+    }
+    distinct_key = {values};
+    set_.insert(distinct_key);
 
-bool DistinctExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+    // values.clear();
+  }
+  // int set_size = static_cast<int>(set_.size());
+  // LOG_DEBUG("set_size == %d",set_size );
+  set_itetator_ = set_.cbegin();
+}
+
+bool DistinctExecutor::Next(Tuple *tuple, RID *rid) {
+  if (set_itetator_ != set_.cend()) {
+    const auto values = (*set_itetator_).values_;
+    auto distint_outschema = plan_->OutputSchema();
+    *tuple = Tuple(values, distint_outschema);
+    set_itetator_++;
+    return true;
+  }
+  return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/distinct_executor.h
+++ src/include/execution/executors/distinct_executor.h
@@ -13,10 +13,46 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
+#include <unordered_set>
 #include <utility>
+#include <vector>
 
+#include "common/util/hash_util.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/distinct_plan.h"
+#include "storage/table/tuple.h"
+#include "type/value.h"
+
+namespace bustub {
+struct DistinKey {
+  std::vector<Value> values_;
+
+  bool operator==(const DistinKey &other) const {
+    for (uint32_t i = 0; i < other.values_.size(); i++) {
+      if (values_[i].CompareEquals(other.values_[i]) != CmpBool::CmpTrue) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+}  // namespace bustub
+
+namespace std {
+template <>
+struct hash<bustub::DistinKey> {
+  std::size_t operator()(const bustub::DistinKey &distinct_key) const {
+    size_t curr_hash = 0;
+    for (const auto &value : distinct_key.values_) {
+      if (!value.IsNull()) {
+        curr_hash = bustub::HashUtil::CombineHashes(curr_hash, bustub::HashUtil::HashValue(&value));
+      }
+    }
+    return curr_hash;
+  }
+};
+}  // namespace std
 
 namespace bustub {
 
@@ -53,5 +89,15 @@ class DistinctExecutor : public AbstractExecutor {
   const DistinctPlanNode *plan_;
   /** The child executor from which tuples are obtained */
   std::unique_ptr<AbstractExecutor> child_executor_;
+
+  // 使用set实现去重， 与 map 和 unorderdmap的区别类似；
+  // 如果使用set则要定义“小于”操作符号，或者将comparotor传入模板参数中； 如果使用unorderd_set, 则要定义 == 和 hash
+  // 既然 hashjoin使用了 unorderedmap，那么这里就使用 set
+
+  // ！！！！ 不对阿， hashjoin 如果使用 map
+  // 那么比较的是一个tuple中的一个joinkey（应该是bustub只支持单个joinkey），但是在distinct中比较的是整个tuple 怎么用 <
+  // 比较一个矢量呢？ 没有定义啊。 还是按照AggregateKey的hash自定义键来处理吧
+  std::unordered_set<DistinKey> set_;
+  std::unordered_set<DistinKey>::const_iterator set_itetator_;
 };
 }  // namespace bustub
